1e6249f8b50b865df4e4fc41712482f6d70ead20
yomayo666 <yomayo1992@yandex.ru>
2024-05-20 20:32:47 +0300

    Updates

--- _nuxt/static/1716225280/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt-posts-app\u002F_nuxt\u002Fstatic\u002F1716225280",layout:"default",error:a,state:{posts:[],users:[],post:{},selectedUser:a,start:0},serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fnuxt-posts-app\u002F",assetsPath:"\u002Fnuxt-posts-app\u002F_nuxt\u002F",cdnURL:a}}}}(null));
